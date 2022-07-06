fd6fa343006b51e2d39c305c988c4cca8d1a425b
gloryFire <nvvquang1504@gmail.com>
2022-07-06 14:42:18 +0700

    update

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const express= require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const feedRoute = require('./routes/feed')
 
-const {mongoConnect} = require('./utils/database')
+
+const mongoose = require('mongoose');
+// const {mongoConnect} = require('./utils/database')
 
 
 const app = express();
 
 app.use(bodyParser.json())
 
-app.use((req,res,next) => {
-   res.setHeader('Access-Control-Allow-Origin','*');
-   res.setHeader('Access-Control-Allow-Method','GET, POST,PUT,PATCH, DELETE');
-   res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization')
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Method', 'GET, POST,PUT,PATCH, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
    next();
 })
 
 
-app.use('/feed',feedRoute)
+app.use('/feed', feedRoute)
 
-
-mongoConnect(() => {
-   app.listen(8080);
-})
+mongoose.connect('mongodb+srv://admin:admin@cluster0.ozxss.mongodb.net/?retryWrites=true&w=majority')
+   .then(rs => {
+      app.listen(8080);
+   })
+// mongoConnect(() => {
+//    app.listen(8080);
+// })
 
--- utils/database.js
+++ utils/database.js
@@ -1,27 +1,27 @@
-const {MongoClient} = require('mongodb');
-
-const url = 'mongodb+srv://admin:admin@cluster0.ozxss.mongodb.net/?retryWrites=true&w=majority'
-
-let _db;
-const mongoConnect = (callback) => {
-   MongoClient.connect(url).then(client => {
-      console.log('Connected')
-      _db = client.db('nvvquang1504');
-      callback();
-   }).catch(err => {
-      console.log(err)
-      throw err;
-   })
-}
-const getDb = () => {
-   if (_db) {
-      return _db;
-   }
-   throw 'No database found'
-}
-
-
-exports.mongoConnect = mongoConnect;
-exports.getDb = getDb;
-
-
+// const {MongoClient} = require('mongodb');
+//
+// const url = 'mongodb+srv://admin:admin@cluster0.ozxss.mongodb.net/?retryWrites=true&w=majority'
+//
+// let _db;
+// const mongoConnect = (callback) => {
+//    MongoClient.connect(url).then(client => {
+//       console.log('Connected')
+//       _db = client.db('nvvquang1504');
+//       callback();
+//    }).catch(err => {
+//       console.log(err)
+//       throw err;
+//    })
+// }
+// const getDb = () => {
+//    if (_db) {
+//       return _db;
+//    }
+//    throw 'No database found'
+// }
+//
+//
+// exports.mongoConnect = mongoConnect;
+// exports.getDb = getDb;
+//
+//
